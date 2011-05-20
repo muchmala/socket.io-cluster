c0cfd596944873056a060f6117c0b91d9d463b09
Taras Kunch <taras.kunch@gmail.com>
2011-05-20 11:55:31 +0000

    Added implementation of frontend server

--- example/config.js
+++ example/config.js
@@ -1,16 +1,1 @@
-var config = exports;
-
-config.HTTP_HOST = '0.0.0.0';
-config.HTTP_PORT = 8080;
-
-config.REDIS_HOST = '127.0.0.1';
-config.REDIS_PORT = 6379;
-config.REDIS_PASSWORD = undefined;
-config.REDIS_DATABASE = 0;
-
-try {
-    var config_local = require('./config.local.js');
-    for (var key in config_local) {
-         config[key] = config_local[key];
-    }
-}catch(e) {}
+../config.js
---
+++ example/frontendServer.js
@@ -0,0 +1,38 @@
+var http = require('http'),
+    url = require('url'),
+    fs = require('fs'),
+    socketIoCluster = require('..'),
+    config = require('./config');
+
+server = http.createServer(function(req, res){
+  // your normal server code
+  var path = url.parse(req.url).pathname;
+  switch (path){
+    case '/':
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write('<h1>Welcome. Try the <a href="/chat.html">chat</a> example.</h1>');
+      res.end();
+      break;
+
+    case '/json.js':
+    case '/chat.html':
+      fs.readFile(__dirname + path, function(err, data){
+        if (err) return send404(res);
+        res.writeHead(200, {'Content-Type': path == 'json.js' ? 'text/javascript' : 'text/html'})
+        res.write(data, 'utf8');
+        res.end();
+      });
+      break;
+
+    //default: send404(res);
+  }
+}),
+
+send404 = function(res){
+  res.writeHead(404);
+  res.write('404');
+  res.end();
+};
+
+server.listen(config.HTTP_PORT);
+socketIoCluster.makeFrontendServer(server, config);
